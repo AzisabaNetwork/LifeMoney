4d50110704f76a1008b8a130b86f369d17141566
azuazu3939 <nnokan0922@gmail.com>
2024-11-12 20:23:14 +0900

    start()

--- src/main/java/net/azisaba/lifemoney/LifeMoney.java
+++ src/main/java/net/azisaba/lifemoney/LifeMoney.java
@@ -23,6 +23,7 @@ public final class LifeMoney extends JavaPlugin implements Task {
     public void onEnable() {
         saveDefaultConfig();
         coinTimer = new CoinLogTimer(this);
+        coinTimer.start();
 
         if (isEconomy()) {
             RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
